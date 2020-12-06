95ca3d92c55757d8f2f38fe13dada3c70d0c4e55
Nicolas <nbortolotti@gmail.com>
2020-12-06 20:13:01 +0100

    TF lite version function
    added a initial function to check the version of tf-lite

---
+++ tflite.go
@@ -0,0 +1,15 @@
+package gotflite
+
+/*
+#include <stdio.h>
+#include <tensorflow/lite/c/c_api.h>
+#cgo LDFLAGS: -ltensorflowlite_c
+#cgo linux LDFLAGS: -ldl -lrt
+*/
+import "C"
+
+// TFVersion ....
+func TFVersion() (string, error) {
+	x := C.TfLiteVersion()
+	return C.GoString(x), nil
+}
