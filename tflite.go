de77d79b64a5e774b801ec478a41cc32bfdcdafb
Nicolas Bortolotti <nbortolotti@gmail.com>
2020-12-15 16:34:48 +0100

    Update tflite.go

--- tflite.go
+++ tflite.go
@@ -4,7 +4,7 @@ package gotflite
 #include <stdio.h>
 #include <tensorflow/lite/c/c_api.h>
 #cgo LDFLAGS: -ltensorflowlite_c
-#cgo linux LDFLAGS: -ldl -lrt
+#cgo linux LDFLAGS: -lm -ldl -lrt
 */
 import "C"
 
